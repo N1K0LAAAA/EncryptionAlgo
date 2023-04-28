c16cddb3c49e1bae392238243b6992a813a74b6e
N1K0LAAAA <114262292+N1K0LAAAA@users.noreply.github.com>
2023-04-28 08:59:17 +0200

    Update NumberDicc.cs
    Code Comments

--- NumberDicc.cs
+++ NumberDicc.cs
@@ -1,37 +1,37 @@
 using System;
 class NumberDicc
 {
-private Dictionary<int, char> dictN;
+private Dictionary<int, char> dictN; // We use a constructor and make a private Dictionary 
 
-   public NumberDicc()
+   public NumberDicc() // Here we make a Dictionary that gives the numbers 0 , 9 a random Char everytime we run it 
     {
     
-     dictN = new Dictionary<int, char>();   
-     Random rnd = new Random(); 
-     for (int i = 0; i <= 9; i++){
-        dictN.Add(((int)i), (char)rnd.Next(65,90));
+     dictN = new Dictionary<int, char>();   // Dictionary with int and char value
+     Random rnd = new Random(); // Random Function
+     for (int i = 0; i <= 9; i++){ // for every int
+        dictN.Add(((int)i), (char)rnd.Next(65,90)); // we add every int and add a random Char
      }
 
     }
 
-    public void printDicc(){
-      foreach(KeyValuePair<int,char> ele in dictN){
-         Console.WriteLine(ele.Key + " " + ele.Value);
+    public void printDicc(){ // Printing the Dictionary Debugg pourpose 
+      foreach(KeyValuePair<int,char> ele in dictN){ // Foreach KeyValue Pair in our Dict 
+         Console.WriteLine(ele.Key + " " + ele.Value); // We print it 
       }
     }
 
-  public List<char> replaceNumberInput(List<int>numbersEncrypt ){
-            List<char> final = new List<char>();
-            for (int i = 0; i < numbersEncrypt.Count; i++){
-            if (dictN.ContainsKey(numbersEncrypt[i])){
-               final.Add(dictN[numbersEncrypt[i]]);
+  public List<char> replaceNumberInput(List<int>numbersEncrypt ){ // Here we take the Numbers that are multiplacted and give them a char
+            List<char> final = new List<char>(); // New char list 
+            for (int i = 0; i < numbersEncrypt.Count; i++){ // for every Number that we multiplicated 
+            if (dictN.ContainsKey(numbersEncrypt[i])){ // we check if contains the Key (0-9)
+               final.Add(dictN[numbersEncrypt[i]]); // and if does we add it to our Dictionary
             }
          }
         
-        for(int i = 0; i < final.Count; i++){
-         Console.WriteLine(final[i]);
+        for(int i = 0; i < final.Count; i++){ // For every Key Pair
+         Console.WriteLine(final[i]); // we print it 
         }
-        return final;
+        return final; // And we run it 
 
        }
   }
