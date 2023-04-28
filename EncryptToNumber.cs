fd1640b23a88387a040988342a61103c84d023df
N1K0LAAAA <114262292+N1K0LAAAA@users.noreply.github.com>
2023-04-28 09:05:33 +0200

    Code Comments

--- EncryptToNumber.cs
+++ EncryptToNumber.cs
@@ -1,34 +1,34 @@
 using System;
 class EncryptToNumber{
 
-    public List<int> encryptNumberList(List<int> numbers)
+    public List<int> encryptNumberList(List<int> numbers) // Here we make Function that takes the ints that we converted from chars to ints
     {
-        List<int> numbersEncrypt = new List<int>();
-        for (int i = 0; i < numbers.Count; i++)
+        List<int> numbersEncrypt = new List<int>(); // Making a new List so we can add the multiplicated Ints
+        for (int i = 0; i < numbers.Count; i++) // For every Int in our Numbers List
         {
-            int calced =0;
+            int calced =0; // Declaring a  empty Int
 
-            if (i == numbers.Count - 1)
+            if (i == numbers.Count - 1) // if we are at the last Element from our List 
             {
 
-                calced += numbers.Last() * numbers[0];
+                calced += numbers.Last() * numbers[0]; // We multiplicate the Last Int from our list and Multiplicate it with the first one 
     
             }
-            else
+            else // Else 
             {
 
-                 calced += numbers[i] * numbers[i + 1];
+                 calced += numbers[i] * numbers[i + 1]; // We just Multiplicate the Int in our List with the Next one 
             }
 
-            numbersEncrypt.Add(calced);
+            numbersEncrypt.Add(calced); // Here we Add every Factor of our multiplicated Numbes 
         }
 
-        for (int i = 0; i < numbersEncrypt.Count; i++)
+        for (int i = 0; i < numbersEncrypt.Count; i++) // for every Int in our List
         {
 
-         Console.WriteLine(numbersEncrypt[i]);
+         Console.WriteLine(numbersEncrypt[i]); // We print it
 
         }
-        return numbersEncrypt;
+        return numbersEncrypt; // And return the Value
     }
 }
