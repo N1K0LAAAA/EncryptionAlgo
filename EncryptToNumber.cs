8fca57bdcc21e8435422dd9d5f57446070ba4fdc
N1K0LAAAA <114262292+N1K0LAAAA@users.noreply.github.com>
2023-04-28 15:57:10 +0200

    message

--- EncryptToNumber.cs
+++ EncryptToNumber.cs
@@ -1,34 +1,31 @@
 using System;
 class EncryptToNumber{
 
-    public List<int> encryptNumberList(List<int> numbers) // Here we make Function that takes the ints that we converted from chars to ints
-    {
-        List<int> numbersEncrypt = new List<int>(); // Making a new List so we can add the multiplicated Ints
-        for (int i = 0; i < numbers.Count; i++) // For every Int in our Numbers List
-        {
-            int calced =0; // Declaring a  empty Int
 
-            if (i == numbers.Count - 1) // if we are at the last Element from our List 
+    // Here We Take every Number and multiplicate it by next one and the last one with the first one
+    public List<int> encryptNumberList(List<int> numbers) 
+    {
+        List<int> numbersEncrypt = new List<int>(); 
+        for (int i = 0; i < numbers.Count; i++) 
+        {
+            int calced =0; 
+
+            if (i == numbers.Count - 1) 
             {
 
-                calced += numbers.Last() * numbers[0]; // We multiplicate the Last Int from our list and Multiplicate it with the first one 
+                calced += numbers.Last() * numbers[0]; 
     
             }
-            else // Else 
+            else 
             {
 
-                 calced += numbers[i] * numbers[i + 1]; // We just Multiplicate the Int in our List with the Next one 
+                 calced += numbers[i] * numbers[i + 1]; 
             }
 
-            numbersEncrypt.Add(calced); // Here we Add every Factor of our multiplicated Numbes 
+            numbersEncrypt.Add(calced); 
         }
 
-        for (int i = 0; i < numbersEncrypt.Count; i++) // for every Int in our List
-        {
 
-         Console.WriteLine(numbersEncrypt[i]); // We print it
-
-        }
-        return numbersEncrypt; // And return the Value
+        return numbersEncrypt; 
     }
 }
--- NumberDicc.cs
+++ NumberDicc.cs
@@ -1,38 +1,62 @@
 using System;
 class NumberDicc
 {
-private Dictionary<int, char> dictN; // We use a constructor and make a private Dictionary 
+    private Dictionary<int, char> dictN;
 
-   public NumberDicc() // Here we make a Dictionary that gives the numbers 0 , 9 a random Char everytime we run it 
+    // Here We Create a Dictionary , The Key is 0 till 9 and the Value are random Letters of the Alphabet
+
+    public NumberDicc()
     {
-    
-     dictN = new Dictionary<int, char>();   // Dictionary with int and char value
-     Random rnd = new Random(); // Random Function
-     for (int i = 0; i <= 9; i++){ // for every int
-        dictN.Add(((int)i), (char)rnd.Next(65,90)); // we add every int and add a random Char
-     }
 
-    }
-
-    public void printDicc(){ // Printing the Dictionary Debugg pourpose 
-      foreach(KeyValuePair<int,char> ele in dictN){ // Foreach KeyValue Pair in our Dict 
-         Console.WriteLine(ele.Key + " " + ele.Value); // We print it 
-      }
-    }
-
-  public List<char> replaceNumberInput(List<int>numbersEncrypt ){ // Here we take the Numbers that are multiplacted and give them a char
-            List<char> final = new List<char>(); // New char list 
-            for (int i = 0; i < numbersEncrypt.Count; i++){ // for every Number that we multiplicated 
-            if (dictN.ContainsKey(numbersEncrypt[i])){ // we check if contains the Key (0-9)
-               final.Add(dictN[numbersEncrypt[i]]); // and if does we add it to our Final List 
-            }
-         }
-        
-        for(int i = 0; i < final.Count; i++){ // For every Key Pair
-         Console.WriteLine(final[i]); // we print it 
+        dictN = new Dictionary<int, char>();
+        Random rnd = new Random();
+        for (int i = 0; i <= 9; i++)
+        {
+            dictN.Add(((int)i), (char)rnd.Next(65, 90));
         }
-        return final; // And we run it 
 
-       }
-  }
+    }
 
+   //Debugging Pourpose 
+
+    public void printDicc()
+    {
+        foreach (KeyValuePair<int, char> ele in dictN)
+        {
+            Console.WriteLine(ele.Key + " " + ele.Value);
+        }
+    }
+
+   // Here we take numbersEncrypt convert them to a string and after converting them to a string
+   // we convert them to a char array and than to int array and at the end we give every int a char 
+    public string replaceNumberInput(List<int> numbersEncrypt)
+    {
+        List<char> final = new List<char>();
+        List<int> toSingleDigits = new List<int>();
+        string toSingleDigitsString = "";
+        for (int i = 0; i < numbersEncrypt.Count; i++)
+        {
+            toSingleDigitsString += numbersEncrypt[i];
+        }
+        char[] toSingleDigitsChar = toSingleDigitsString.ToCharArray();
+        int[] toSingleDigitsInt = new int[toSingleDigitsChar.Length];
+        for (int i = 0; i < toSingleDigitsChar.Length; i++)
+        {
+            toSingleDigitsInt[i] = Convert.ToInt32(toSingleDigitsChar[i].ToString());
+        }
+        for (int i = 0; i < toSingleDigitsInt.Length; i++)
+        {
+            if (dictN.ContainsKey((toSingleDigitsInt[i])))
+            {
+                final.Add(dictN[(toSingleDigitsInt[i])]);
+            }
+        }
+         string encryptText = "";
+        for (int i = 0; i < final.Count; i++)
+        {
+            encryptText += final[i];
+            
+        }
+        return encryptText;
+    }
+}
